8e2259172f594df69c60c9348768f57e7a00fe3b
Iurii Kozuliak <i.kozuliak@gmail.com>
2016-07-19 22:23:12 +1000

    Initial commit

---
+++ app/containers/App/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import styles from './styles.css';
+
+export default class App extends React.Component { // eslint-disable-line react/prefer-stateless-function
+
+  static propTypes = {
+    children: React.PropTypes.node,
+  };
+
+  render() {
+    return (
+      <div className={styles.container}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
